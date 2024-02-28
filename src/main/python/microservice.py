89cefff65e7b05dcf3cb3f60a3a372ba77232474
Ananda Patel <anandaapatel@gmail.com>
2024-02-28 19:13:13 +0530

    doc string added

--- src/main/python/microservice.py
+++ src/main/python/microservice.py
@@ -1,11 +1,23 @@
+"""
+Microservice Module
+This module defines a simple microservice using Flask, a lightweight web framework for Python.
+It provides two endpoints:
+- /api/hello: Returns a JSON response with a greeting message.
+- /api/add: Accepts two numbers as query parameters and returns their sum.
 
-import requests
+"""
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 @app.route('/api/add', methods=['GET'])
 
 def add():
+    """
+    Handle GET request for /api/add endpoint.
+    Accepts two numbers as query parameters and returns their sum.
+    Returns:
+        JSON response with the sum of two numbers.
+    """
     num1 = int(request.args.get('num1'))
     num2 = int(request.args.get('num2'))
     return jsonify({'result': num1 + num2})
